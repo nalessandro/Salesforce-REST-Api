09a1880795a00497a093247cce85223dcba2e69f
Nathan Alessandro <nalessan@gmail.com>
2017-10-20 17:19:08 -0400

    Request

--- src/Client.php
+++ src/Client.php
@@ -23,15 +23,22 @@ class Client
 
     /**
      * Client constructor.
+     *
      * @param String $login_url
      * @param String $username
      * @param String $password
      * @param String $client_id
      * @param String $client_secret
+     * @param String $security_token
      */
-    public function __construct(String $login_url, String $username, String $password, String $client_id, String $client_secret)
-    {
-        $config = new ClientConfig($login_url, $username, $password, $client_id, $client_secret);
+    public function __construct(String $login_url
+                                ,String $username
+                                ,String $password
+                                ,String $client_id
+                                ,String $client_secret
+                                ,String $security_token
+    ) {
+        $config = new ClientConfig($login_url, $username, $password, $client_id, $client_secret, $security_token);
         $this->rest = new Rest($config);
         $this->bulk = new Bulk($config);
     }
